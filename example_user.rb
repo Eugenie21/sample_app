43489098d332507a24702368910527eaca087cdd
Eugenie21 <eugenie2116@mail.ru>
2014-12-02 15:06:59 +0300

    Finish layout and routes

---
+++ example_user.rb
@@ -0,0 +1,13 @@
+class User
+
+	attr_accessor :name, :email
+
+	def initialize(attrributes = {})
+		@name = attrributes[:name]
+		@email = attrributes[:email]
+	end
+
+	def formatted_email
+		"#{@name} <#{@email}>"
+	end 
+end
